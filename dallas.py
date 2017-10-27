d2b501b77dec0201953db4c64bc845a45c29343a
Anton Malcher <anton@busow36.de>
2017-10-27 16:56:28 +0000

    read show and save sensor data from dallas sensor

--- dallas.py
+++ dallas.py
@@ -1,7 +1,15 @@
-
+import dataService
 
 def readSensor(sensorId):
     file = open("/sys/bus/w1/devices/{:s}/w1_slave".format(sensorId))
     data = file.read()
     file.close()
     return float(data.split("\n")[1].split(" ")[9][2:]) / 1000
+
+def printValues(sensorId, temperature):
+    print("{:s}: Temperature={:0.1f} degree ".format(sensorId, temperature))
+
+def readShowAndSave(sensorId):
+    data = readSensor(sensorId)
+    printValues(sensorId, data)
+    dataService.addSensorData(sensorId, "temperature", data)
--- main.py
+++ main.py
@@ -5,6 +5,6 @@ import dallas
 
 #while 1:
 #    dht.readShowAndSave("dht11-2", "DHT11", 2)
-#    dht.readShowAndSave("dht22-3", "DHT22", 3)
+#   dht.readShowAndSave("dht22-3", "DHT22", 3)
     #time.sleep(5)
-dallas.readSensor("28-041700e4fcff")
+dallas.readShowAndSave("28-041700e4fcff")
