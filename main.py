30e53fde0651a26d0343456d2a5502fc1fe54536
Anton Malcher <anton@busow36.de>
2017-10-27 21:12:40 +0000

    add new sensors (new DHT22)

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import time
 import dht
 import dallas
 
-
 while 1:
-    dht.readShowAndSave("dht11-2", "DHT11", 2)
+    dht.readShowAndSave("dht11-17", "DHT11", 17)
+    dht.readShowAndSave("dht22-2", "DHT22", 2)
     dht.readShowAndSave("dht22-3", "DHT22", 3)
     dallas.readShowAndSaveAll()
     time.sleep(5)
